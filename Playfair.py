aec54ca6dcc5d9560dfa99258bb714662f426a49
William Ray Johnson <willrayjohnson@gmail.com>
2017-09-03 17:50:37 -0500

    Added method to split message into pairs

--- Playfair.py
+++ Playfair.py
@@ -2,3 +2,23 @@
 @author William Ray Johnson
 9/3/2017
 '''
+
+import PlayfairTable
+
+class Playfair:
+    
+    
+    def splitMessage(self, message):
+        message = message.lower().replace(" ", "")
+        splitMessage = ""
+        
+        for pair in range(len(message)//2):
+            splitMessage = splitMessage + message[:2] + " "
+            message = message[2:]
+        
+        if len(message) == 1:
+            splitMessage = splitMessage + message + "x"
+        elif len(message) == 0:
+            splitMessage = splitMessage.rstrip()
+            
+        return splitMessage
--- PlayfairTest.py
+++ PlayfairTest.py
@@ -9,5 +9,23 @@ import Playfair
 
 class PlayfairTest(unittest.TestCase):
     
+    def setUp(self):
+        self.cipher = Playfair.Playfair()
+    
+    def testSplitMessageWithOddLengthMessage(self):
+        message = "This is a test"
+        expectedSplit = "th is is at es tx"
+        actualSplit = self.cipher.splitMessage(message)
+        
+        self.assertEqual(actualSplit, expectedSplit)
+        
+    def testSplitMessageWithEvenLengthMessage(self):
+        message = "Hello World"
+        expectedSplit = "he ll ow or ld"
+        actualSplit = self.cipher.splitMessage(message)
+        
+        self.assertEqual(actualSplit, expectedSplit)
+        
+        
 if __name__ == '__main__':
     unittest.main()
